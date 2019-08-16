714c1ff7d8b6e96d22c42158897c7abd4c59638f
gitlGl <43978320+gitlGl@users.noreply.github.com>
2019-08-17 01:28:40 +0800

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -96,8 +96,8 @@ DATABASES = {
       'default': {
       'ENGINE': 'django.db.backends.mysql',
      'NAME':'blog',
-     'USER': 'root',
-      'PASSWORD': 'lingl.',
+     'USER': 'test',
+      'PASSWORD': '95461284',
       'HOST': 'localhost',
       'PORT': '3306',
       }
