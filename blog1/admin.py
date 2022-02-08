2709bf961e07b6ed46bcc6aad959af9f531e86c8
gitlG <1637885653@qq.com>
2022-02-08 23:45:08 +0800

    测试

--- blog1/admin.py
+++ blog1/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from .models import Post, Category
-
+#测试
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
 
